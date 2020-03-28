4f80c8b0baa3ddcb439d708e4fe7cc2531cb3e41
Ioannis Maliouris <maliouri@csd.auth.gr>
2020-03-28 14:05:01 +0200

    First lesson of typescript

--- merged/src/app/app-routing.module.ts
+++ merged/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ContactComponent } from './contact/contact.component';
+import { SignComponent } from './sign/sign.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'contact',
     component: ContactComponent
+  },
+  {
+    path: 'sign',
+    component: SignComponent
   },
   {
     path: '',
--- merged/src/app/app.module.ts
+++ merged/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
+import { SignComponent } from './sign/sign.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContactComponent } from './contact/contact.component';
     HomepageComponent,
     HeaderComponent,
     FooterComponent,
-    ContactComponent
+    ContactComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule,
