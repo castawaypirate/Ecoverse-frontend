02b3d7a92c82442d0b1eaecfc9310871fb36abcf
Ioannis Maliouris <maliouri@csd.auth.gr>
2020-04-27 22:52:01 +0300

    Change title of news page

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -11,7 +11,7 @@ export class NewsComponent implements OnInit {
   constructor(private title: Title, private meta: Meta) { }
 
   ngOnInit(): void {
-    this.title.setTitle('Announcements');
+    this.title.setTitle('News');
     this.meta.updateTag({ name: 'description', content: 'Events, Announcements, Articles, Warnings, News Page'});
   }
 
